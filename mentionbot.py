5f8ac9a71768e7db5437b9324a63062d1ba587ba
MyBagofTricks <themiddlemanisbetter@hotmail.com>
2015-03-08 23:55:45 -0700

    Added some base changes

--- mentionbot.py
+++ mentionbot.py
@@ -1,6 +1,19 @@
-"""  To do:
-- Add optional plain text file support instead of MySQL
-- Add option to send an account a notification via reddit message."""
+#!/usr/bin/python
+
+
+"""
+################################
+          Mentionbot
+###############################
+
+This is Mentionbot, a simple script which scans reddit.com for
+keywords and stores them in a database.
+
+LICENSED UNDER GPL. See LICENSE for details. 
+
+
+"""
+
 import praw 
 import MySQLdb
 from time import sleep, localtime, strftime
@@ -8,6 +21,7 @@ from sys import argv
 from configobj import ConfigObj
 config = ConfigObj('mentionbot.setup')
 subid_array = []
+usesql = True
 keywords = config['keywords']
 dbhost = config['dbhost']
 dbuser = config['dbuser']
@@ -20,73 +34,103 @@ subname = config['subname']
 user_agent = config['user_agent']
 time_sleep = int(config['time_sleep'])
 
-def mysqlempty():
-    clear = raw_input('[-] Clear the database? [y/n]?: ')
-    if clear in ('y','Y','yes','YES'):
-        try:
-            db = MySQLdb.connect(dbhost,dbuser,dbpass,dbname,\
-                charset='utf8')
-            cursor = db.cursor()
-            cursor.execute("DELETE FROM %s"%dbtable)
-            db.commit()
-            db.close()
-        except Exception, e:
-            print "[x] Error = "+str(e)
-    else:
-        print "[-] Not clearing database"
+class MySQL:
 
-def mysqlpopulate():
-    try:
-        print('*'*72+"\n[-] Populating existing thread(s) "+
-            "from %s >> %s"%(dbname,dbtable))
-        db = MySQLdb.connect(dbhost,dbuser,dbpass,dbname,\
-            charset='utf8')
+    def addpost(self, id, title, link):
+        print "[+] POST ADDED! | %s | %s..." % (link, title[0:27])
+        subid_array.append(id)
+        db = MySQLdb.connect(dbhost, dbuser, dbpass, dbname, charset='utf8')
         cursor = db.cursor()
-        cursor.execute("SELECT * FROM %s"%dbtable)
-        results = cursor.fetchall()
-        for row in results:
-            subid = row[0]
-            subid_array.append(subid)
+        cursor.execute(
+            "INSERT INTO %s(subid, title, link) VALUES(%%s, %%s, %%s)"
+            % dbtable, (id, title, link))
+        db.commit()
         db.close()
-        print('*'*72+"\n[+] %s database loaded. %s post(s) already "\
-            +"populated.")%(dbname,len(subid_array))
-    except Exception, e:
-        print "[x] Error = "+str(e)
- 
+
+    @staticmethod
+    def empty():
+        clear = raw_input('[-] Clear the database? [y/n]?: ')
+        if clear in ('y', 'Y', 'yes', 'YES'):
+            try:
+                db = MySQLdb.connect(
+                    dbhost, dbuser, dbpass, dbname, charset='utf8')
+                cursor = db.cursor()
+                cursor.execute("DELETE FROM %s" % dbtable)
+                db.commit()
+                db.close()
+            except Exception, e:
+                print "[x] Error = " + str(e)
+        else:
+             print "[-] Not clearing database"
+
+    @staticmethod 
+    def populate():
+        try:
+            print('*' * 72 +"\n[-] Populating existing thread(s) from %s >> %s"
+                    % (dbname, dbtable))
+            db = MySQLdb.connect(dbhost, dbuser, dbpass, dbname, charset='utf8')
+            cursor = db.cursor()
+            cursor.execute("SELECT * FROM %s" % dbtable)
+            results = cursor.fetchall()
+            for row in results:
+                subid = row[0]
+                subid_array.append(subid)
+            db.close()
+            print('*'*72+"\n[+] %s database loaded. %s post(s) already "
+                    + "populated.") % (dbname, len(subid_array))
+        except Exception, e:
+             print "[x] Error = " + str(e)
+
+
+class Text:
+
+    @staticmethod
+    def empty():
+        print "This only appears when empting text databases. No sql"
+
+    @staticmethod
+    def populate():
+        print "This should only appear when populating array, no sql."
+
+
+def initialize_db():
+    if usesql is True:
+        MySQL.empty()
+        MySQL.populate()
+    else:
+        Text.empty()
+        Text.populate()
+
+
 def run_bot():
-    print "[-] "+strftime("%a, %y-%m-%d %H:%M:%S %Z ",localtime())+\
-        "Scanning /r/%s for keyword(s)"%subname
+    print ("[-] " + strftime("%a, %y-%m-%d %H:%M:%S %Z ", localtime())
+            + "Scanning /r/%s for keyword(s)" % subname)
     subreddit = r.get_subreddit(subname)
     for sub in subreddit.get_new(limit=100):
         title_text = sub.selftext.title()
         has_keyword = any(string in title_text for string in keywords)
-        if sub.id not in subid_array and has_keyword:
-            print "[+] POST ADDED! | %s | %s..." % \
-                (sub.short_link,sub.title[0:27])
-            subid_array.append(sub.id)
-            db = MySQLdb.connect(dbhost,dbuser,dbpass,dbname,\
-                charset='utf8')
-            cursor = db.cursor()
-            cursor.execute("INSERT INTO %s(subid, title, link) VALUES\
-                (%%s,%%s,%%s)"%dbtable,(sub.id,sub.title,\
-                sub.short_link))
-            db.commit()
-            db.close()
+        if sub.id not in subid_array and has_keyword and usesql is True:
+            post = MySQL()
+            post.addpost(sub.id,sub.title,sub.short_link)
+        elif sub.id not in subid_array and has_keyword and usesql is False:
+            print "This message shouldn't get triggered..."
+        else:
+            pass
 
-# Program Begins 
 print ('*'*72+"""
 
 *    Welcome to MentionBot0.3. I scan reddit for keywords and save 
 *    them to a database.
 
 """+'*'*72+"\n")
+
 r = praw.Reddit(user_agent)
-r.login(redlogin,redpass)
-mysqlempty()
-mysqlpopulate()
+r.login(redlogin, redpass)
+initialize_db()
+
 while True:
     try:
         run_bot()
     except Exception, e:
-        print "[x] Error = "+str(e)
+        print "[x] Error = " + str(e)
     sleep(time_sleep)
